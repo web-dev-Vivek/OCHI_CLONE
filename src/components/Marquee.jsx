95d3eb83fb16875facda5a271feaa1e619d5058e
web_dev_vivek <progamervivek2020@gmail.com>
2025-04-19 18:40:15 +0530

    4th commit

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,9 +4,9 @@ function Marquee() {
 return (
     <div className='w-full py-10 bg-[#004d43]'>
             <div className="text border-t-2 border-b-2 gap-[5vw] tracking-tighter flex whitespace-nowrap overflow-hidden border-zinc-300">
-                    <h1 className='text-[10vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase font-semibold' style={{ letterSpacing: '-0.04em' }}>we are ochi</h1>
-                    <h1 className='text-[10vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase font-semibold' style={{ letterSpacing: '-0.04em' }}>we are ochi</h1>
-                    <h1 className='text-[10vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase font-semibold' style={{ letterSpacing: '-0.04em' }}>we are ochi</h1>
+                    <h1 className='text-[20vw] leading-none font-["FoundersGroteskCond-Lt.otf"] uppercase font-semibold' style={{ letterSpacing: '-0.04em' }}>we are ochi</h1>
+                    <h1 className='text-[20vw] leading-none font-["FoundersGroteskCond-Lt.otf"] uppercase font-semibold' style={{ letterSpacing: '-0.04em' }}>we are ochi</h1>
+                    <h1 className='text-[20vw] leading-none font-["FoundersGroteskCond-Lt.otf"] uppercase font-semibold' style={{ letterSpacing: '-0.04em' }}>we are ochi</h1>
             </div>
     </div>
 )
