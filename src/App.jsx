767230fddedc8d914473eb966d7792450642bf9d
web_dev_vivek <progamervivek2020@gmail.com>
2025-04-22 14:21:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Landingpage from './components/Landingpage'
 import Marquee from './components/Marquee'
 import About from './components/about'
+import Eyes from './components/Eyes'
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Landingpage/>
       <Marquee/>
       <About/>
+      <Eyes/>
     </div>
   )
 }
