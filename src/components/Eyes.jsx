ffe698a1a20e4e6f7469e822199420bbfac2ffc5
web_dev_vivek <progamervivek2020@gmail.com>
2025-04-26 21:10:24 +0530

    updated

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 
 function Eyes() {
     const[rotate , setRotate] = useState(0);
 
     useEffect(() => {
-        window.addEventListener('mousemove', (e) => {
+        const handleMouseMove = (e) => {
             let mouseX = e.clientX;
             let mouseY = e.clientY;
-
+    
             let deltaX = mouseX - window.innerWidth / 2;
             let deltaY = mouseY - window.innerHeight / 2;
-
+    
             let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-            setRotate(angle-180);
-        })
-    })
+            setRotate(angle - 180);
+        };
+    
+        window.addEventListener('mousemove', handleMouseMove);
+    
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+    
 
   return (
     <div className='eyes w-full h-screen overflow-hidden'>
         <div className='relative w-full h-full bg-cover  bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'>
-        <div className='absolute flex gap-10 top-1/6 left-1/2 -translate-x-[50%] -trans-ty-[50%]'>
+        <div className='absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
             <div className='w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-zinc-100'>
                 <div className='w-2/3 h-2/3 rounded-full relative bg-zinc-900'>
-                    <div style={{rotate{${rotate}degree}}} className='absolute top-1/2  -traslate-x-[50%] -translate-y-[50%] line w-full h-10'>
+                <div style={{ transform: `rotate(${rotate}deg)` }} className='absolute top-[4vw] line w-full h-10'>
+
                         <div className='w-10 h-10 rounded-full bg-zinc-100 '></div>
                     </div>
                 </div>
             </div>
             <div className='w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-zinc-100'>
-            <div className='w-2/3 h-2/3 rounded-full relative bg-zinc-900'>
-                    <div className='absolute top-1/2  -traslate-x-[50%] -translate-y-[50%]  line w-full h-10'>
+                <div className='w-2/3 h-2/3 rounded-full relative bg-zinc-900'>
+                <div style={{ transform: ` rotate(${rotate}deg)` }} className='absolute  top-[4vw] line w-full h-10'>
+
                         <div className='w-10 h-10 rounded-full bg-zinc-100 '></div>
                     </div>
                 </div>
