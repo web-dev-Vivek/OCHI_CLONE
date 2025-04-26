5df46a3a1401389f6f001614f41a75f7392601ec
web_dev_vivek <progamervivek2020@gmail.com>
2025-04-26 20:46:03 +0530

    updated

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
 function Eyes() {
+    const[rotate , setRotate] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener('mousemove', (e) => {
+            let mouseX = e.clientX;
+            let mouseY = e.clientY;
+
+            let deltaX = mouseX - window.innerWidth / 2;
+            let deltaY = mouseY - window.innerHeight / 2;
+
+            let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+            setRotate(angle-180);
+        })
+    })
+
   return (
     <div className='eyes w-full h-screen overflow-hidden'>
         <div className='relative w-full h-full bg-cover  bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'>
         <div className='absolute flex gap-10 top-1/6 left-1/2 -translate-x-[50%] -trans-ty-[50%]'>
             <div className='w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-zinc-100'>
                 <div className='w-2/3 h-2/3 rounded-full relative bg-zinc-900'>
-                    <div className='absolute top-1/2  -traslate-x-[50%] -translate-y-[50%] line w-full h-10'>
+                    <div style={{rotate{${rotate}degree}}} className='absolute top-1/2  -traslate-x-[50%] -translate-y-[50%] line w-full h-10'>
                         <div className='w-10 h-10 rounded-full bg-zinc-100 '></div>
                     </div>
                 </div>
